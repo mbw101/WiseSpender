200184fdb705ebda57c940cd5fabd56a5483264f
Malcolm Wright <wrightmbw01@gmail.com>
2024-02-28 07:37:18 -0500

    Got the correct layout needed for the Initial screen!

--- screens/InitialScreen.tsx
+++ screens/InitialScreen.tsx
@@ -11,30 +11,23 @@ import { getCurrentDate } from "./helpers";
 
 function InitialScreen() {
     return (
-        // <View style={styles.container}>
-        //     {/* <Text style={styles.dateStyle}>{getCurrentDate()}</Text> */}
-        //     <View>
-        //         <Text>Add a monthly goal to get started...</Text>
-        //         {/* TODO: Add monthly goal button (the plus sign) */}
-        //         <Image
-        //             source={require("./../assets/add.png")}
-        //             style={styles.imageStyle} />
-        //     </View>
-        // </View>
         <View style={styles.container}>
-            <Text>Add a monthly goal to get started...</Text>
-            {/* TODO: Add monthly goal button (the plus sign) */}
-            <Image
-                source={require("./../assets/add.png")}
-                style={styles.imageStyle} />
+            <Text style={styles.dateStyle}>{getCurrentDate()}</Text>
+            <View style={styles.addGoal}>
+                <Text>Add a monthly goal to get started...</Text>
+                {/* TODO: Add monthly goal button (the plus sign) */}
+                <Image
+                    source={require("./../assets/add.png")}
+                    style={styles.imageStyle} />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: "center", // center vertically
-        alignItems: "center",
+        // alignItems: "center",
+        padding: 5,
         flex: 1,
         flexGrow: 1,
         backgroundColor: "red"
@@ -42,11 +35,10 @@ const styles = StyleSheet.create({
     addGoal: {
         justifyContent: "center", // center vertically
         alignItems: "center", // center horizontally
-        // width: "100%",
+        width: "100%",
         flex: 1,
-        flexDirection: 'row',
-        // textAlign: "center",
-        color: "black"
+        flexGrow: 1,
+        backgroundColor: "green"
     },
     dateStyle: {
         color: "#0C9AEA",
