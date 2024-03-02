48c59fade8bf8ddf86f701cfde250cf8ded8e918
Malcolm Wright <wrightmbw01@gmail.com>
2024-03-02 08:38:09 -0500

    Revert "Merge Leo into Malcolm's branch"

--- App.tsx
+++ App.tsx
@@ -1,50 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
 
-import { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import 'react-native-gesture-handler';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import { getUpdateString, getCurrentDate } from './screens/helpers';
+import InitialScreen from './screens/InitialScreen';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
 
-import NavigateComponent from './Components/Navigator.tsx';
-
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View,} from 'react-native';
-import HomeScreen from './Screens/HomeScreen.tsx';
-import ActivityScreen from './Screens/ActivityScreen.tsx';
-import HistoryScreen from './Screens/HistoryScreen.tsx';
-import ProfileScreen from './Screens/ProfileScreen.tsx';
-
-const TabNavigator = createBottomTabNavigator();
-const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
+  console.log(getUpdateString());
+  console.log(getCurrentDate());
 
   return (
-    <NavigationContainer>
-      <TabNavigator.Navigator screenOptions={{
-        tabBarActiveTintColor:"blue",
-        tabBarInactiveTintColor:"#fff",
-        tabBarStyle:{backgroundColor:"#000"},
-        tabBarLabelStyle: {
-          fontSize: 15},
-        }}>
-        <TabNavigator.Screen name="Home" component={HomeScreen}/>
-        <TabNavigator.Screen name="Archive" component={HistoryScreen}/>
-        <TabNavigator.Screen name="Activity" component={ActivityScreen}/>
-        <TabNavigator.Screen name="Profile" component={ProfileScreen}/>
-      </TabNavigator.Navigator>
-    </NavigationContainer> 
+    // TODO: Check if initial goal has been set (state management)
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor={Colors.white}
+      />
+      <InitialScreen />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
     flexDirection: 'row',
     height: "100%",
     width: "100%"
